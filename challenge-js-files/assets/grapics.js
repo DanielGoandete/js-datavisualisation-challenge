297076c22272aba85fbe890504ea359a7be9a48d
nataliacherman08 <cherman.natalia08@gmail.com>
2021-03-08 12:20:25 +0100

    Update

--- challenge-js-files/assets/grapics.js
+++ challenge-js-files/assets/grapics.js
@@ -1,7 +1,7 @@
-let parentElement = document.getElementById("table1");
+let parentElement = document.getElementById("Crimes_et_d.C3.A9lits_enregistr.C3.A9s_par_les_services_de_police");
 
 let firstChild = parentElement.firstChild;
 
 let newElement = document.createElement("canvas");
 
-parentElement.insertBefore(newElement, firstChild);
+parentElement.insertBefore(newElement, firstChild.nextSibling);
