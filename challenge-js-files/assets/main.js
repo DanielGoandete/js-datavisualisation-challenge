98f2be7f8edcc34f9522a5e035852e8fc0bd66d9
nataliacherman08 <cherman.natalia08@gmail.com>
2021-03-08 11:32:03 +0100

    Update

--- challenge-js-files/assets/main.js
+++ challenge-js-files/assets/main.js
@@ -1,1 +1,10 @@
 let element = document.createElement("canvas");
+new element.id = "canvas";
+
+let text = document.createTextNode("my canvas zone");
+
+element.appendChild(text);
+
+let table1 = document.querySelector("#table1");
+
+document.body.insertBefore(element, table1);
