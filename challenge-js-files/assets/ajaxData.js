98632ac43920a0dc1bd38fcf1779d4c82ae91fd6
DanielGoandete <63291047+DanielGoandete@users.noreply.github.com>
2021-03-09 09:32:44 +0100

    update

--- challenge-js-files/assets/ajaxData.js
+++ challenge-js-files/assets/ajaxData.js
@@ -1,7 +1,7 @@
 Chart.defaults.global.defaultFontSize = 16;
 Chart.defaults.global.defaultFontColor = "#000";
 
-let chart1 = () => {
+let graphiqueChart = () => {
     let canvas1 = document.createElement("canvas");
     canvas1.setAttribute("id", "canvas");
     canvas1.setAttribute("height", "300");
@@ -9,38 +9,38 @@ let chart1 = () => {
     document.getElementById("firstHeading").appendChild(canvas1);
 
     let canvas = document.getElementById("canvas").getContext("2d");
-    let dataPoints = [];
+    let dataChart = [];
     let label = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     let i = 9;
     let chart = new Chart(canvas, {
-        type: 'line',//bar, horizontalBar, pie, line , doughnut , radar ,polarArea
+        type: 'bar',//bar, horizontalBar, pie, line , doughnut , radar ,polarArea
         data: {
             labels: label,
             datasets: [{
                 label: ["Crime Statistics' Chart"],
-                borderColor: "red",
-                data: dataPoints,
+                data: dataChart,
+                //borderColor: "red",
             }]
         }
     })
 
-    // Update and ajax
-    function updateGraph() {
+    // Update graphique by time
+    function graphiqueUpdateByTime() {
         i++;
         label.push(i);
 
-
+        
         fetch('https://canvasjs.com/services/data/datapoints.php')
-            .then(response => {
-                return response.json()
+            .then(res => {
+                return res.json()
             })
             .then(data => {
-                dataPoints.length + 1;
-                data.forEach(element => dataPoints.push(parseInt(element[1])));
+                dataChart.length + 1;
+                data.forEach(ele => dataChart.push(parseInt(ele[1])));
                 chart.update();
             })
     };
-    setInterval(updateGraph, 2500);
+    setInterval(graphiqueUpdateByTime, 2500);
 }
 
-chart1();
+graphiqueChart();
