e1e556b7903d240377230fe92dbb9636888fd179
DanielGoandete <63291047+DanielGoandete@users.noreply.github.com>
2021-03-09 17:25:25 +0100

    update

--- challenge-js-files/assets/ajaxData.js
+++ challenge-js-files/assets/ajaxData.js
@@ -19,7 +19,7 @@ let graphiqueChart = () => {
             datasets: [{
                 label: ["Crime Statistics' Chart"],
                 data: dataChart,
-                //borderColor: "red",
+                borderColor: "red",
             }]
         }
     })
@@ -46,106 +46,6 @@ graphiqueChart();
 
 
 
-
-// let table = document.getElementById("table1");
-// var j = [].reduce.call(table.rows, function(res, row) {
-//     res[row.cells[0].textContent.slice(0,-1)] = row.cells[1].textContent;
-//     return res
-// }, {});
-
-// document.querySelector("pre").textContent = JSON.stringify(j, null, 2);
-
-
-/*function tableToJSON(x) {
-    let x;
-    let obj = {};
-    let row, rows = table.rows;
-    for (let i = 0; i <  rows.length; i++) {
-        row = rows[i];
-        obj[row.cells[0].textContent] = row.cells[1].textContent;
-    }
-    document.querySelector("x").textContent = JSON.stringify(obj);
-    return JSON.stringify(obj);
-}
-
-tableToJSON();
-Table1
-Table2*/
-
-let _table = document.getElementById("table1");
-let _trLength = _table.getElementsByTagName("tr").length;
-let _jsonData = [];
-let _obj = {};
-
-let _table1ToJSON = function (index) {
-    let _tr = _table.getElementsByTagName("tr")[index];
-    let _td = _tr.getElementsByTagName("td");
-    let _arr = [].map.call(_td, function (td) {
-        return td.innerHTML;
-    }).join(',');
-    let _data = _arr.split(",");
-
-    _obj = {
-        country: _data[0]
-        , 2002: _data[1]
-        , 2003: _data[2]
-        , 2004: _data[3]
-        , 2005: _data[4]
-        , 2006: _data[5]
-        , 2007: _data[6]
-        , 2008: _data[7]
-        , 2009: _data[8]
-        , 2010: _data[9]
-        , 2011: _data[10]
-        , 2012: _data[11]
-    };
-
-    _jsonData.push(_obj);
-
-};
-
-let _table2 = document.getElementById("table2");
-let _trLength2 = _table2.getElementsByTagName("tr").length;
-let _jsonData2 = [];
-let _obj2 = {};
-let _table2ToJSON = function (index) {
-    
-    let _tr2 = _table2.getElementsByTagName("tr")[index];
-    let _td2 = _tr2.getElementsByTagName("td");
-    let _arr2 = [].map.call(_td2, function (td) {
-        return td.innerHTML;
-    }).join(',');
-    let _data2 = _arr2.split(",");
-
-    _obj2 = {
-        country: _data2[0]
-        , 2002: _data2[1]
-        , 2003: _data2[2]
-        , 2004: _data2[3]
-        , 2005: _data2[4]
-        , 2006: _data2[5]
-        , 2007: _data2[6]
-        , 2008: _data2[7]
-        , 2009: _data2[8]
-        , 2010: _data2[9]
-        , 2011: _data2[10]
-        , 2012: _data2[11]
-    };
-
-    _jsonData2.push(_obj2);
-
-};
-
-for (let i = 1; i < _trLength; i++) {
-    _table1ToJSON(i);
-   
-}
-for (let i = 1; i < _trLength2; i++) {
-    
-    _table2ToJSON(i);
-}
-//document.getElementById("table1").textContent = JSON.stringify(_jsonData);
-//document.getElementById("table2").textContent = JSON.stringify(_jsonData2);
 function tableToJson(table) {
     let data = [];
     for (i = 1; i < table.rows.length; i++) {
@@ -161,25 +61,72 @@ function tableToJson(table) {
 
 
 let year = [2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012]
-let dataTable = {};
 let labelTable  = document.getElementById("table1");
-let country = [];
-let valueCountry = [],[];
+let data2 = [];
 let table = Array.from(tableToJson(labelTable));
-for(let i =1;i < table.length; i++){
-    for(let g=1; g < table[i].length; g++){
 
-        valueCountry[g].push(table[g][1]);
-    }
-
-    country.push(table[i][0]);
+for (let j = 1; j < table.length; j++) { 
+    let sum = []; 
+    for (let i = 1; i < table[j].length; i++) {
+         if (parseFloat(table[j][i])) {
+              sum.push(parseFloat(table[j][i])); 
+        } 
+    } data2[table[j][0]] = sum; 
 }
 
-console.log(country);
-console.log(valueCountry);
 
-console.log(table);
-
+console.log(Object.keys(data2)[1]);
+console.log(Object.values(data2)[1]);
 
 
 
+
+let graphique1 = () => {
+    let canvas2 = document.createElement("canvas");
+    canvas2.setAttribute("id", "canvas2");
+    canvas2.setAttribute("height", "300");
+    canvas2.setAttribute("width", "600");
+    document.getElementById("firstHeading").appendChild(canvas2);
+
+    let canvasC = document.getElementById("canvas2").getContext("2d");
+    let dataChart = [];
+    let label = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    let i = 9;
+    let chart = new Chart(canvasC, {
+        type: 'bar',//bar, horizontalBar, pie, line , doughnut , radar ,polarArea
+        data: {
+            labels: label,
+            datasets: [{
+                label: Object.keys(data2)[1],
+                data: Object.values(data2)[1],
+                borderColor: "red",
+            }]
+        }
+    })
+}
+graphique1();
+
+let graphique2 = () => {
+    let canvas2 = document.createElement("canvas");
+    canvas2.setAttribute("id", "canvas2");
+    canvas2.setAttribute("height", "300");
+    canvas2.setAttribute("width", "600");
+    document.getElementById("firstHeading").appendChild(canvas2);
+
+    let canvasC = document.getElementById("canvas2").getContext("2d");
+    let dataChart = [];
+    let label = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    let i = 9;
+    let chart = new Chart(canvasC, {
+        type: 'bar',//bar, horizontalBar, pie, line , doughnut , radar ,polarArea
+        data: {
+            labels: label,
+            datasets: [{
+                label: Object.keys(data2)[1],
+                data: Object.values(data2)[1],
+                borderColor: "red",
+            }]
+        }
+    })
+}
+graphique2();
